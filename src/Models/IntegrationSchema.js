9dd2a5f6f713586dbbb55b42fa60d5a901add3bf
magnuspedro <pedmagnus@gmail.com>
2020-02-17 22:11:17 -0300

    Making better code for pipedrive integration

--- src/Models/IntegrationSchema.js
+++ src/Models/IntegrationSchema.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
 const IntegrationSchema = new mongoose.Schema({
-  pagination: {
-    start: Number,
-    limit: Number,
-    more_items_in_collection: Boolean,
-    next_start: Number
-  },
+  start: Number,
+  limit: Number,
+  more_items_in_collection: Boolean,
+  next_start: Number,
   created_at: { type: Date, default: new Date() }
 })
 
--- src/Services/Pipedrive/Pipedrive.js
+++ src/Services/Pipedrive/Pipedrive.js
@@ -5,32 +5,21 @@ const IntegrationSchema = require('../../Models/IntegrationSchema')
 
 class Pipedrive {
   async getWonDeal () {
-    let start = 0
     let response
-    let limit = 100
     const transactionDao = new TransactionDao()
     const pipedriveRequest = new PipedriveRequest(
       'deals',
       env.get('PIPEDRIVE_KEY').asString()
     )
 
-    let pagination = await IntegrationSchema.findOne(
-      {},
-      {},
-      {
-        sort: { created_at: -1 }
-      }
-    )
-
-    if (pagination) {
-      pagination = pagination.pagination
-      if (!pagination.more_items_in_collection) {
-        start = pagination.start
-      } else {
-        start = pagination.next_start
-      }
-      limit = pagination.limit
-    }
+    let { start = 0, limit = 100 } =
+      (await IntegrationSchema.findOne(
+        {},
+        {},
+        {
+          sort: { created_at: -1 }
+        }
+      )) || ''
 
     do {
       response = await pipedriveRequest.get({
@@ -41,7 +30,7 @@ class Pipedrive {
       transactionDao.create(response.data)
       start++
     } while (response.additional_data.pagination.more_items_in_collection)
-    IntegrationSchema.create(response.additional_data)
+    IntegrationSchema.create(response.additional_data.pagination)
     return response
   }
 }
